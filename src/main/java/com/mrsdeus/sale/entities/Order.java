2f95389b2d624109537b5f1f3cbdc7b2a4bcfc6a
hiagodonha <hiagodefenix@gmail.com>
2025-07-01 20:46:46 -0300

    add lombok annotations

--- src/main/java/com/mrsdeus/sale/entities/Order.java
+++ src/main/java/com/mrsdeus/sale/entities/Order.java
@@ -1,13 +1,23 @@
 package com.mrsdeus.sale.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 import java.util.UUID;
 
-
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-@Table(name = "order")
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue
     private UUID id;
 }
